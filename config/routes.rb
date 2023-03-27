3947a011a7342144f6846dc1f892c2f03fcfea87
Alejandra Landa <aleriquene@gmail.com>
2023-03-27 16:07:55 +0000

    started working on directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors",{:controller=>"directors", :action=>"index"})
+  get("/actors",{:controller=>"directors", :action=>"index"})
+  get("/movie",{:controller=>"directors", :action=>"index"})
+  get("/character",{:controller=>"directors", :action=>"index"})
 end
