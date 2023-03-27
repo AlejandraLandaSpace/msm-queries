1b795d15824bbc817dbb160cbe7c1a4910700a73
Alejandra Landa <aleriquene@gmail.com>
2023-03-27 16:54:49 +0000

    director show details created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors",{:controller=>"directors", :action=>"index"})
+  get("/directors/eldest",{:controller=>"directors", :action=>"wisest"})
   get("/actors",{:controller=>"directors", :action=>"index"})
   get("/movie",{:controller=>"directors", :action=>"index"})
   get("/character",{:controller=>"directors", :action=>"index"})
