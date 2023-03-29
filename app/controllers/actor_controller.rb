e5b4419a8f9166dfb8d80720efcf8685415dfd5d
Alejandra Landa <aleriquene@gmail.com>
2023-03-29 15:42:07 +0000

    finally finished

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -1,17 +1,18 @@
 class ActorController < ApplicationController
 def index
-  @all_actors_list = Actor.all #returns a list of all actors
+  @all_actors_list = Actor.all 
   render({:template=>"actor_template/index.html.erb"})
 end
 def actor_details
+  @list_of_actors = Actor.all
+  @list_of_characters = Character.all
+  @list_of_movies = Movie.all
   actor_id = params.fetch("actor_id")
-  @actor = Actor.where({:id => actor_id}).at(0)
-
-  characters = Character.where({ :actor_id => actor_id })
-   list_of_movie_ids = characters.map_relation_to_array(:movie_id)
-
-  @filmography = Movie.where({:id=>list_of_movie_ids})
-
-  render({:template=>"actor_template/actor_details.html.erb"})
+  character = Character.where({ :actor_id => actor_id})
+  list_of_movie_ids = character.map_relation_to_array(:movie_id)
+  @actor = @list_of_actors.where({ :id => actor_id }).at(0)
+  @filmography = Movie.where({ :id => list_of_movie_ids})
+  @character = Character.where({ :actor_id => @actor})
+  render({ :template => "actor_template/actor_details.html.erb"})
 end
 end
